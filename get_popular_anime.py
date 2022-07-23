e693ef100bbef6ed3e8c93482371995d6f79fe18
Sheepposu <49356627+Sheepposu@users.noreply.github.com>
2022-07-23 14:56:51 -0400

    define hxh years

--- get_popular_anime.py
+++ get_popular_anime.py
@@ -21,16 +21,19 @@ for _ in range(10):
     ranking += r
 
 
-def format_title(title):
+def format_title(anime):
+    title = anime.title if anime.alternative_titles.en is None or anime.alternative_titles.en == "" \
+        else anime.alternative_titles.en
+    if anime.title == "Hunter x Hunter (2011)":
+        title = anime.title
+    elif anime.title == "Hunter x Hunter":
+        title = "Hunter x Hunter (1999)"
     return "".join([char for char in title if char.isascii()])
 
 
 with open("data/anime.json", "w") as f:
     json.dump(list(map(
         lambda r: (
-            format_title(r.anime.alternative_titles.en
-                         if r.anime.alternative_titles.en is not None and
-                            r.anime.alternative_titles.en != ""
-                         else r.anime.title)
+            format_title(r.anime)
         ),
         ranking)), f)
