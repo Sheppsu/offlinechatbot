d36ff8789ddb3a8c4619b38c772b3e187d2f8eae
Sheppsu <apc2005osu@gmail.com>
2024-05-31 15:40:38 -0500

    refactor anime

--- anime.py
+++ anime.py
@@ -1,25 +1,5 @@
-from mal import Client
 from constants import name_formatting
-import os
 import json
-import sys
-
-
-if "--test" in sys.argv or "--local" in sys.argv:
-    from dotenv import load_dotenv
-    load_dotenv()
-
-
-client = Client.from_client_credentials(os.getenv("MAL_CLIENT_ID"), os.getenv("MAL_CLIENT_SECRET"))
-
-paging = None
-ranking = []
-for _ in range(10):
-    if paging is None:
-        r, paging = client.get_anime_ranking("bypopularity", fields="title,alternative_titles{en}")
-    else:
-        r, paging = paging.get_next(client)
-    ranking += r
 
 
 def format_title(anime):
@@ -34,7 +14,19 @@ def format_title(anime):
     return "".join([name_formatting[char] if char in name_formatting else char for char in title if char.isascii()])
 
 
-def create_list():
+def create_list(client):
+    paging = None
+    ranking = []
+    for _ in range(10):
+        try:
+            if paging is None:
+                r, paging = client.get_anime_ranking("bypopularity", fields="title,alternative_titles{en}")
+            else:
+                r, paging = paging.get_next(client)
+        except:
+            return
+        ranking += r
+
     with open("data/anime.json", "w") as f:
         json.dump(list(map(
             lambda r: (
@@ -44,4 +36,10 @@ def create_list():
 
 
 if __name__ == "__main__":
-    create_list()
+    from dotenv import load_dotenv
+    load_dotenv()
+
+    from mal import Client
+    import os
+    client = Client.from_client_credentials(os.getenv("MAL_CLIENT_ID"), os.getenv("MAL_CLIENT_SECRET"))
+    create_list(client)
